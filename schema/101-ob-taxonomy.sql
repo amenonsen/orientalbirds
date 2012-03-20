aeefe5348ced9952cf3e847fd0f61e58ca4d3a9e
Abhijit Menon-Sen <ams@toroid.org>
2012-03-20 09:27:24 +0530

    A set of tables to store taxonomic data

---
+++ schema/101-ob-taxonomy.sql
@@ -0,0 +1,39 @@
+-- One row per taxonomic rank (Kingdom, Phylum, Class, etc.)
+
+create table ranks (
+    rank_id serial primary key,
+    rank_name text not null unique
+);
+
+-- One row per taxon (but genera and species are handled specially by
+-- the next table, though they could in principle be stored here).
+
+create table taxa (
+    taxon_id serial primary key,
+    rank_id integer not null references ranks,
+    parent integer references taxa(taxon_id),
+    taxon_name text not null unique,
+    taxon_description text
+);
+
+-- One row per species, with the generic and specific names stored
+-- separately, and the family identified using the above table.
+
+create table species (
+    species_id serial primary key,
+    family integer not null references taxa(taxon_id),
+    genus text not null,
+    species text not null,
+    common_name text not null,
+    unique (genus, species)
+);
+
+-- One row per alternative common or scientific name for a species
+-- (language_id is 1 for "Latin", or 2 for English).
+
+create table alternative_names (
+    species_id integer not null references species,
+    language_id integer not null
+        constraint valid_language check (language_id in (1,2)),
+    alt_name text not null
+);
