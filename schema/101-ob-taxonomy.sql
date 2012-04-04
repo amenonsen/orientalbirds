c55c7f2066ee47c6d85d4e28d0f828c6f4354093
Abhijit Menon-Sen <ams@toroid.org>
2012-04-04 18:21:38 +0530

    Add checklist tables

--- schema/101-ob-taxonomy.sql
+++ schema/101-ob-taxonomy.sql
@@ -37,3 +37,21 @@ create table alternative_names (
         constraint valid_language check (language_id in (1,2)),
     alt_name text not null
 );
+
+-- One row per checklist (e.g. Howard and Moore). Just a list of names.
+
+create table checklists (
+    checklist_id serial primary key,
+    name text not null unique,
+    description text
+);
+
+-- One row per species per checklist, identifying the serial number of
+-- the species in the given checklist. (We don't support "1024A", just
+-- an integer for now.)
+
+create table checklist_species (
+    checklist_id integer not null references checklists,
+    species_id integer not null references species,
+    serial integer not null,
+);
