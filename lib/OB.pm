e5ef866f0e4cef0b61933f326d9d5e03b49407a9
Abhijit Menon-Sen <ams@toroid.org>
2012-11-07 13:50:46 +0530

    Add a jumble of routes

--- lib/OB.pm
+++ lib/OB.pm
@@ -20,14 +20,26 @@ sub startup {
 
     $r->get('/')->to(template => 'index');
 
+    my $auth = $app->plugin('login');
+    my $secure = $r->find('secure');
+
+    $secure->get('/login')->to(template => 'auth/login');
+
     $r->get('/upload')->to('upload#index');
     $r->post('/upload')->to('upload#upload');
 
-    $r->any(
-        '/' => sub {
-            shift->render_text("Hello world!", format => 'txt')
-        }
-    );
+    my $id = qr/[1-9][0-9]*/;
+    $r->get('/contributors')->to('contributors#index');
+    $r->get('/contributors/form')->to('contributors#form');
+    $r->post('/contributors/save')->to('contributors#save');
+    $r->route('/contributors/:contributor_id', contributor_id => $id)
+        ->via('get')->to('contributors#profile');
+
+    my $admin = $auth->allow_roles('admin');
+
+    $admin->get('/users')->to('users#index');
+    $admin->get('/users/form')->to('users#form');
+    $admin->post('/users/save')->to('users#save');
 }
 
 1;
