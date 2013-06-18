3345f25d1be1c57063d42c9ea956a7c36aab6c4e
Abhijit Menon-Sen <ams@toroid.org>
2013-06-18 17:08:09 +0530

    An initial list of countries

---
+++ schema/192-location-data.sql
@@ -0,0 +1,30 @@
+COPY countries(country_code, country_name) FROM STDIN;
+BD	Bangladesh
+BT	Bhutan
+BN	Brunei
+KH	Cambodia
+OT	Chagos Archipelago
+CN	China
+HK	Hong Kong
+IN	India
+ID	Indonesia
+JP	Japan
+KP	Korea North
+KR	Korea South
+LA	Laos
+MY	Malaysia
+MV	Maldives
+MN	Mongolia
+MM	Myanmar (Burma)
+NP	Nepal
+PK	Pakistan
+PH	Philippines
+RU	Russia (east of 90°)
+SG	Singapore
+SL	Sri Lanka
+TW	Taiwan
+TH	Thailand
+TI	Tibet
+TL	Timor-Leste (East Timor)
+VN	Vietnam
+\.
